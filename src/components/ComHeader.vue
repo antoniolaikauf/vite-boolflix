dbf0558addbbf8201fd29fd5659aa87377154c61
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-01-10 12:54:42 +0100

    add: aggiunto componenti main e header pe progetto

---
+++ src/components/ComHeader.vue
@@ -0,0 +1,10 @@
+<script >
+export default{
+    newCopm:"comHeader"
+}
+</script>
+
+<template>
+</template>
+
+<style scoped lang="scss"></style>
---
+++ src/components/ComMain.vue
@@ -0,0 +1,11 @@
+<script >
+export default{
+    newCopm:"comMain"
+}
+</script>
+
+<template>
+    
+</template>
+
+<style scoped lang="scss"></style>
