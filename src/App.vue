8378d8d3db8121e2b2e75b639a9e022333acaa0b
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-01-10 13:52:50 +0100

    add: sistemato ricerca del film in base a quello che cerca utente

--- src/App.vue
+++ src/App.vue
@@ -5,40 +5,40 @@ import comHeader from './components/ComHeader.vue';
 // componente main 
 import comMain from './components/ComMain.vue'
 
-import{store} from './store'
+import { store } from './store'
 
 export default {
   components: {
     comHeader,
     comMain
   },
-  data(){
-    return{
+  data() {
+    return {
       store
     }
   },
-  methods:{
-    richiestaapi(){
-      axios.get(store.linkAPI)
-      .then((risposta)=>{
-        console.log(risposta.data);
-      })
-      .catch((err)=>{
-        console.log("ritornato errore" + err);
-      })
+  methods: {
+    richiestaapi() {
+      let urlAPI = store.linkAPI
+      urlAPI += `&query=${store.searchFilm}`
+      axios.get(urlAPI)
+        .then((risposta) => {
+          console.log(risposta.data);
+        })
+        .catch((err) => {
+          console.log("ritornato errore" + err);
+        })
+      console.log(store.searchFilm);
     }
   },
-  created(){
+  created() {
     this.richiestaapi()
   }
-
-
-
 }
 </script>
 
 <template>
-  <comHeader />
+  <comHeader @searchfilm="richiestaapi" />
   <main class="bg-secondary">
     <div class="container">
       <div class="row">
--- src/components/ComHeader.vue
+++ src/components/ComHeader.vue
@@ -1,18 +1,27 @@
 <script >
+import { store } from '../store';
 export default {
-    newCopm: "comHeader"
+    newCopm: "comHeader",
+
+    data() {
+        return {
+            store
+        }
+    }
 }
+
 </script>
 
 <template>
     <nav class="navbar navbar-expand-sm navbar-light bg-light p-1 bg-dark">
-            <a class="navbar-brand text-danger" href="#">BOOLFIX</a>
-            <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">              
-                <form class="d-flex ">
-                    <input class="form-control me-2 " type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success bg-primary text-light" type="submit">Search</button>
-                </form>
-            </div>
+        <a class="navbar-brand text-danger" href="#">BOOLFIX</a>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+            <form class="d-flex ">
+                <input class="form-control me-2 " type="search" placeholder="Search" aria-label="Search"
+                    v-model="store.searchFilm">
+                <button class="btn btn-outline-success bg-primary text-light" type="submit" @click.prevent="$emit('searchfilm')">Search</button>
+            </form>
+        </div>
     </nav>
 </template>
 
